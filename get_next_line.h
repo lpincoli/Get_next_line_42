434400dc83325d5be1fca944c1d56efaa221589e
Lorenzo Pincolini <lpincoli@c1r2p3.42firenze.it>
2023-06-19 15:05:07 +0200

    tolto main e file di testo per testing

--- get_next_line.h
+++ get_next_line.h
@@ -10,6 +10,10 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 42
+#endif
+
 #ifndef GET_NEXT_LINE_H
 
 # define GET_NEXT_LINE_H
